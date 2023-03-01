f4d3c1ee389dc8e6e5e5c7d78f69d1d50e69715b
rana271 <ranacomp@gmail.com>
2023-03-01 10:46:09 +0530

    Add project files.

---
+++ XamarinAPI/Controllers/PostController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+namespace XamarinAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : Controller
+    {
+
+        private List<PostModel> list = new List<PostModel>();
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public List<PostModel> Get()
+        {
+            //Ado.net Data Access 
+            //List<PostModel> list = new List<PostModel>();
+            //SqlConnection con = new SqlConnection(@"Data Source=RANA\SQLEXPRESS;Initial Catalog=XamarinDB;Integrated Security=True");
+            //con.Open();
+            //SqlCommand cmd = new SqlCommand("Select * from Post", con);
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //while (dr.Read())
+            //{
+            //    int id = Convert.ToInt32(dr["Id"]);
+            //    int userId = Convert.ToInt32(dr["UserId"]);
+            //    list.Add(new PostModel { Id = id, UserId = userId, Title = dr["Title"].ToString(), Description = dr["Description"].ToString() });
+
+            //    //list.Add(new PostModel { Id = 1, UserId = 1, Title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", Description = "My desc1" }
+            //    //    );
+            //    //list.Add(new PostModel { Id = 2, UserId = 1, Title = "qui est esse", Description = "My desc2" }
+            //    //        );
+
+            //}
+            list.Add(new PostModel { Id = 1, UserId = 1, Title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", Description = "My desc1" }
+                    );
+            list.Add(new PostModel { Id = 2, UserId = 1, Title = "qui est esse", Description = "My desc2" }
+                    );
+
+            return list;
+        }
+    }
+}
---
+++ XamarinAPI/PostModel.cs
@@ -0,0 +1,12 @@
+﻿namespace XamarinAPI
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
